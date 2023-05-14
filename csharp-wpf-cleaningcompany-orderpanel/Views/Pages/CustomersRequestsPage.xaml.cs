c05953e7bbad1f1635929f178b3f762feec686b5
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-05-14 22:01:30 +0300

    chore: Created page for processing customers requests in <CustomersRequestsPage.xaml>.

--- csharp-wpf-cleaningcompany-orderpanel/Views/Pages/CustomersRequestsPage.xaml.cs
+++ csharp-wpf-cleaningcompany-orderpanel/Views/Pages/CustomersRequestsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
 {
@@ -7,6 +9,23 @@ namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
         public CustomersRequestsPage()
         {
             InitializeComponent();
+        }
+
+        private void RefreshTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                MessageBox.Show("Fail to refresh or display table!");
+            }
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
