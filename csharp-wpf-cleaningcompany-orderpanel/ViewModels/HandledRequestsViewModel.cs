99c59f0f6c0053d1714207b61acf72947678c75c
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-05-16 17:58:11 +0300

    chore: Implemented loading and displaying handled requests in DataGrid from the database through <HandledRequestsViewModel.cs>

---
+++ csharp-wpf-cleaningcompany-orderpanel/ViewModels/HandledRequestsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using csharp_wpf_cleaningcompany_orderpanel.Models;
+
+namespace csharp_wpf_cleaningcompany_orderpanel.ViewModels
+{
+    class HandledRequestsViewModel : ViewModelBase
+    {
+        private ObservableCollection<HandledRequest> handledRequests;
+        public ObservableCollection<HandledRequest> HandledRequests
+        {
+            get { return handledRequests; }
+            set
+            {
+                handledRequests = value;
+                OnPropertyChanged(nameof(HandledRequests));
+            }
+        }
+
+        public async Task FillDataGrid()
+        {
+            using (var context = new HandledRequestContext())
+            {
+                var requests = await Task.Run(() =>
+                    context.HandledRequests.OrderByDescending(cr => cr.Id).ToList());
+                HandledRequests = new ObservableCollection<HandledRequest>(requests);
+            }
+        }
+    }
+}
--- csharp-wpf-cleaningcompany-orderpanel/Views/Pages/HandledRequestsPage.xaml.cs
+++ csharp-wpf-cleaningcompany-orderpanel/Views/Pages/HandledRequestsPage.xaml.cs
@@ -1,20 +1,26 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using csharp_wpf_cleaningcompany_orderpanel.ViewModels;
 
 namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
 {
     public partial class HandledRequestsPage : Page
     {
+        private HandledRequestsViewModel handledRequestsViewModel;
+
         public HandledRequestsPage()
         {
             InitializeComponent();
+            handledRequestsViewModel= new HandledRequestsViewModel();
+            DataContext = handledRequestsViewModel;
+            _ = handledRequestsViewModel.FillDataGrid();
         }
 
-        private void RefreshTableButton_Click(object sender, RoutedEventArgs e)
+        private async void RefreshTableButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-
+                await handledRequestsViewModel.FillDataGrid();
             }
             catch
             {
