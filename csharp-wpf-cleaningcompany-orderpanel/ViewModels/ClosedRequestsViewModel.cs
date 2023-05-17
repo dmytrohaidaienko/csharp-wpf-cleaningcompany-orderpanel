50b64ba8af603d1ed6858c7a0b9f8df6534ef971
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-05-17 15:18:20 +0300

    chore: Implemented loading and displaying closed requests in DataGrid from the database through <ClosedRequestsViewModel.cs>.

---
+++ csharp-wpf-cleaningcompany-orderpanel/ViewModels/ClosedRequestsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using csharp_wpf_cleaningcompany_orderpanel.Models;
+using csharp_wpf_cleaningcompany_orderpanel.ViewModels.AppContext;
+
+namespace csharp_wpf_cleaningcompany_orderpanel.ViewModels
+{
+    class ClosedRequestsViewModel : ViewModelBase
+    {
+        private ObservableCollection<ClosedRequest> closedRequests;
+
+        public ObservableCollection<ClosedRequest> ClosedRequests
+        {
+            get { return closedRequests; }
+            set
+            {
+                closedRequests = value;
+                OnPropertyChanged(nameof(ClosedRequests));
+            }
+        }
+
+        public async Task FillDataGrid()
+        {
+            using (var context = new ClosedRequestContext())
+            {
+                var requests = await Task.Run(() =>
+                    context.ClosedRequests.OrderByDescending(cr => cr.Id).ToList());
+                ClosedRequests = new ObservableCollection<ClosedRequest>(requests);
+            }
+        }
+    }
+}
--- csharp-wpf-cleaningcompany-orderpanel/Views/Pages/ClosedRequestsPage.xaml.cs
+++ csharp-wpf-cleaningcompany-orderpanel/Views/Pages/ClosedRequestsPage.xaml.cs
@@ -1,18 +1,31 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using csharp_wpf_cleaningcompany_orderpanel.ViewModels;
 
 namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
 {
     public partial class ClosedRequestsPage : Page
     {
+        private ClosedRequestsViewModel closedRequestsViewModel;
+
         public ClosedRequestsPage()
         {
             InitializeComponent();
+            closedRequestsViewModel= new ClosedRequestsViewModel();
+            DataContext= closedRequestsViewModel;
+            _= closedRequestsViewModel.FillDataGrid();
         }
 
-        private void RefreshTableButton_Click(object sender, RoutedEventArgs e)
+        private async void RefreshTableButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                await closedRequestsViewModel.FillDataGrid();
+            }
+            catch
+            {
+                MessageBox.Show("Fail to refresh or display table!");
+            }
         }
     }
 }
