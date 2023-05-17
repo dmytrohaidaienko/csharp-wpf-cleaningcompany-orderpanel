a3562228e0523b4af90043049eef8565eb45275e
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-05-17 14:50:29 +0300

    chore: Created page for closed requests in <ClosedRequestsPage.xaml>.

--- csharp-wpf-cleaningcompany-orderpanel/Views/Pages/ClosedRequestsPage.xaml.cs
+++ csharp-wpf-cleaningcompany-orderpanel/Views/Pages/ClosedRequestsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
 {
@@ -7,6 +8,11 @@ namespace csharp_wpf_cleaningcompany_orderpanel.Views.Pages
         public ClosedRequestsPage()
         {
             InitializeComponent();
+        }
+
+        private void RefreshTableButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
